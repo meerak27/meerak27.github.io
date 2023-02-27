76ff870f125bbf582ce4e14fa4f2a7f6e3474228
meerak27 <98391104+meerak27@users.noreply.github.com>
2023-02-27 08:14:20 +0400

    .

---
+++ Assignment5/script.js
@@ -0,0 +1,5 @@
+let insideOfBridge = document.getElementById("Inside")
+
+insideOfBridge.addEventListener("click", () => {
+	document.getElementById("textbox").innerText = "You've Clicked on the inside of the Jade Belt Bridge!"
+})
